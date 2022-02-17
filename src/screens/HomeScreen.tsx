60522189c786ae43fdfe325618f0db68d41060fd
Franklin Jara <franklinjaradev@gmail.com>
2022-02-16 23:57:37 -0500

    chore: adding prop types for navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import React from "react";
 
-export default function HomeScreen({ navigation }: any) {
+export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
   return (
     <View>
       <Text>HomeScreen</Text>
--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 
-export default function PlannerScreen({ navigation }: any) {
+export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
   return (
     <View>
       <Text>PlannerScreen</Text>
