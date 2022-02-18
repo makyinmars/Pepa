d9ad0c27980fa3320fe580b895761b6c7b9007cc
Franklin Jara <franklinjaradev@gmail.com>
2022-02-17 20:20:40 -0500

    chore: adding oswald font

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
 
+import * as Font from "expo-font";
+
 export default function useCachedResources() {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
 
   useEffect(() => {
-    const loadResourcesAndDataAsync = () => {
-      setTimeout(() => {
+    const loadResourcesAndDataAsync = async () => {
+      try {
+        await Font.loadAsync({
+          oswald: require("../../assets/fonts/Oswald-Regular.ttf"),
+          "oswald-bold": require("../../assets/fonts/Oswald-Bold.ttf"),
+        });
+      } catch (error) {
+        console.warn(error);
+      } finally {
         setIsLoadingComplete(true);
-      }, 3000);
+      }
     };
 
     loadResourcesAndDataAsync();
