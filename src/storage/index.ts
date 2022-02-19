dde0f452ce4ff5ef1a8023e3078925d8ccf1f088
Franklin Jara <franklinjaradev@gmail.com>
2022-02-19 18:04:06 -0500

    chore: function to clearData

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -29,3 +29,11 @@ export const containsKey = async (key: string) => {
     console.log(error);
   }
 };
+
+export const removeData = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/storage/workout.ts
+++ src/storage/workout.ts
@@ -1,5 +1,5 @@
 import data from "../data/data.json";
-import { containsKey, getData, storeData } from ".";
+import { containsKey, getData, removeData, storeData } from ".";
 import { Workout } from "../types/data";
 
 export const getWorkouts = async (): Promise<Workout[]> => {
@@ -10,9 +10,12 @@ export const getWorkouts = async (): Promise<Workout[]> => {
 export const initWorkouts = async (): Promise<boolean> => {
   const hasWorkouts = await containsKey("workout-data");
   if (!hasWorkouts) {
-    console.log("Storing data");
     await storeData("workout-data", data);
     return true;
   }
   return false;
+};
+
+export const clearWorkouts = async (): Promise<void> => {
+  await removeData("workout-data");
 };
