7d9308c033f55f45e1cbf37db668fa449df72ea6
Franklin Jara <franklinjaradev@gmail.com>
2022-02-19 19:12:14 -0500

    chore: adding useFocused to refresh data

--- src/hooks/useWorkouts.ts
+++ src/hooks/useWorkouts.ts
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+
 import { getWorkouts } from "../storage/workout";
 import { Workout } from "../types/data";
 
 export const useWorkouts = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     const getData = async () => {
+      console.log("Getting data");
       const workouts = await getWorkouts();
       setWorkouts(workouts);
     };
-    getData();
-  }, []);
+    if (isFocused) {
+      getData();
+    }
+  }, [isFocused]);
 
   return workouts;
 };
