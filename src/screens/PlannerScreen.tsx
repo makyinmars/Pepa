207632f915be6d527978da8587ece24f2f6ca2dc
Franklin Jara <franklinjaradev@gmail.com>
2022-03-06 22:30:00 -0500

    chore: adding item component

--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { useState } from "react";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import slugify from "slugify";
 
 import ExerciseForm, { ExerciseFormData } from "../components/ExerciseForm";
 import { SequenceItem, SequenceType } from "../types/data";
+import ExerciseItem from "../components/ExerciseItem";
 
 export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
   const [seqItems, setSeqItems] = useState<SequenceItem[]>([]);
@@ -30,14 +31,7 @@ export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
       <ExerciseForm onSubmit={handleFormSubmit} />
       <FlatList
         data={seqItems}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text>Workout: {item.name}</Text>
-            <Text>Duration: {item.duration}</Text>
-            <Text>{item.reps && `Reps: ${item.reps}`}</Text>
-            <Text>Type: {item.type}</Text>
-          </View>
-        )}
+        renderItem={({ item }) => <ExerciseItem item={item} />}
         keyExtractor={(item) => item.slug}
       />
     </View>
@@ -48,14 +42,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-  },
-  item: {
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    borderWidth: 1,
-    marginVertical: 8,
-    padding: 8,
-    flex: 1,
-    alignItems: "center",
   },
 });
