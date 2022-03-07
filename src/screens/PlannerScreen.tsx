0567db71af2796de4ac196e97d190a27f006e7ff
Franklin Jara <franklinjaradev@gmail.com>
2022-03-06 22:18:55 -0500

    chore: displaying data for new exercise

--- src/screens/PlannerScreen.tsx
+++ src/screens/PlannerScreen.tsx
@@ -1,4 +1,5 @@
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useState } from "react";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import slugify from "slugify";
 
@@ -6,6 +7,8 @@ import ExerciseForm, { ExerciseFormData } from "../components/ExerciseForm";
 import { SequenceItem, SequenceType } from "../types/data";
 
 export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
+  const [seqItems, setSeqItems] = useState<SequenceItem[]>([]);
+
   const handleFormSubmit = (form: ExerciseFormData) => {
     const sequenceItem: SequenceItem = {
       slug: slugify(form.name + " " + Date.now(), { lower: true }),
@@ -18,12 +21,25 @@ export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
       sequenceItem.reps = Number(form.reps);
     }
 
+    setSeqItems([...seqItems, sequenceItem]);
     console.log(sequenceItem);
   };
 
   return (
     <View style={styles.container}>
       <ExerciseForm onSubmit={handleFormSubmit} />
+      <FlatList
+        data={seqItems}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>Workout: {item.name}</Text>
+            <Text>Duration: {item.duration}</Text>
+            <Text>{item.reps && `Reps: ${item.reps}`}</Text>
+            <Text>Type: {item.type}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.slug}
+      />
     </View>
   );
 }
@@ -32,5 +48,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+  },
+  item: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    borderWidth: 1,
+    marginVertical: 8,
+    padding: 8,
+    flex: 1,
+    alignItems: "center",
   },
 });
